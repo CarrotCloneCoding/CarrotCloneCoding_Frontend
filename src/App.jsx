9c69890ca42f44b871b3b3c01e7d100c588b5fa7
baekseojin <seojinbaek100@gmail.com>
2023-06-15 20:40:20 +0900

    FEAT :: 카드 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Main from "./components/Main/Main.jsx";
+import Card from "./components/Card/Card";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}></Route>
+        <Route path="/kr/nearby-stores/" element={<></>}></Route>
+        <Route path="articles/598273895" element={<Card />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Section/Section5/Section5.jsx
+++ src/components/Section/Section5/Section5.jsx
@@ -1,7 +1,13 @@
+import Card from "../../Card/Card";
+import "./Section5.css";
+
 export default function Section5() {
   return (
     <div className="section5">
       <h1>중고거래 인기매물</h1>
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
